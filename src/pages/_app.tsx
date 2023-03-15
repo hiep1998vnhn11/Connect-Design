ed5589800c08bb6b461a73794f2bcd66a162c3ef
hiep1998vnhn11 <hiep1998vnhn11@gmail.com>
2023-03-15 15:06:44 +0700

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,12 @@ import { NextIntlProvider } from 'next-intl';
 import '../styles/main.css';
 import '../styles/prism-a11y-dark.css';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
+const MyApp = ({
+  Component,
+  pageProps,
+}: AppProps<{
+  messages: Record<string, string>;
+}>) => (
   <NextIntlProvider messages={pageProps.messages}>
     <Component {...pageProps} />
   </NextIntlProvider>
